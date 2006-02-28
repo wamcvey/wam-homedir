aa46c57cbff7f6f835c74fc4b25ed87efc71b958
wam <wam@fb92aec8-b207-0410-9ae9-f7dd3fcc407c>
2006-02-28 04:09:40 +0000

    Added commandline options for logging, including -v for varying verbosity. Added better logic for finding the module we're testing.
    git-svn-id: svn+ssh://svn.wamber.net/svn/trunk@63 fb92aec8-b207-0410-9ae9-f7dd3fcc407c

--- .new/test_*.py
+++ .new/test_*.py
@@ -13,8 +13,20 @@ __revision__ =	"$Id:$"
 import os
 import sys
 import unittest
+import inspect
 
-sys.path.insert(1,"..")		# assumes we're being run from a 'tests' subdir
+def filename_in_testdir(filename):
+	"""Return the pathname to the specified file relative to the 
+	directory that this unittest was found in. (Good for locating
+	datafiles)
+	"""
+	return os.path.join(
+		os.path.dirname(inspect.getfile(filename_in_testdir)),
+		filename)
+
+# assumes this test module is located in 'tests' subdir relative to 
+# module being tested
+sys.path.insert(1, filename_in_testdir(".."))		
 import @(TESTED_MODULE)
 
 class Test_@(TESTED_MODULE)(unittest.TestCase):
@@ -34,10 +46,10 @@ class Test_@(TESTED_MODULE)(unittest.TestCase):
 	#	self.failIf( expr[, msg])
 	#	self.fail([msg])
 
-def test():
+def test(verbosity=0):
 	suite = unittest.TestSuite()
 	suite.addTest(unittest.makeSuite(Test_@(TESTED_MODULE)))
-	unittest.TextTestRunner(verbosity=2).run(suite)
+	unittest.TextTestRunner(verbosity=verbosity).run(suite)
 
 if __name__ == '__main__':
 	import sys
@@ -45,11 +57,23 @@ if __name__ == '__main__':
 	import logging
 	from optparse import OptionParser       # aka Optik
 
+	# set up commandline arguments
+	Progname=os.path.basename(sys.argv[0])
+	Usage="%prog usage: @(TESTED_MODULE) [-v [-v [-v]]]\n" \
+	      "%prog usage: -h\n" \
+	      "%prog usage: -V" 
+	optparser = OptionParser(usage=Usage, version="%prog: $Id:$" )
+	optparser.remove_option("--version")    # we add our own that knows -V
+	optparser.add_option("-V", "--version", action="version",
+	  help="show program's version number and exit")
+	optparser.add_option("-v", "--verbose", dest = "verbose",
+	  action="count", help="be verbose (each -v adds more verbosity)")
+	(options, params) = optparser.parse_args(sys.argv[1:])
+
 	root_log = logging.getLogger()          # grab the root logger
 	handler = logging.StreamHandler()
 	logformat = "%(name)s: %(levelname)s: %(message)s"
 	handler.setFormatter(logging.Formatter(logformat))
 	root_log.addHandler(handler)
 
-	test()
-
+	test(verbosity=options.verbose)
