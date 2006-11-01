85fb3805a215bd861a288ab713eb8706838943a1
wam <wam@fb92aec8-b207-0410-9ae9-f7dd3fcc407c>
2006-11-01 16:12:55 +0000

    Added template for greasemonkey scripts
    git-svn-id: svn+ssh://svn.wamber.net/svn/wam-homedir/trunk@398 fb92aec8-b207-0410-9ae9-f7dd3fcc407c

---
+++ dot-new/*.user.js
@@ -0,0 +1,13 @@
+@"""// ==UserScript==
+// @name          Hello World
+// @namespace     http://diveintogreasemonkey.org/download/
+// @description   example script to alert "Hello world!" on every page
+// @include       *
+// @exclude       http://diveintogreasemonkey.org/*
+// @exclude       http://www.diveintogreasemonkey.org/*
+// ==/UserScript==
+"""
+// Greasemonkey user script to do ZZZ
+// @(NAME)
+// @(DAY) @(MONTH), @(YEAR)
+//
