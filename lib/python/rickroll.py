2f8ae50383361be6da6ce64120d5d9ae635ef8a4
William McVey <wam@cisco.com>
2008-12-29 18:23:53 -0500

    Initial checkin

---
+++ lib/python/rickroll.py
@@ -0,0 +1,3 @@
+print "\n".join("Arire tbaan %s" % a for a in ["tvir lbh hc,", "yrg lbh qbja,", "eha nebhaq naq qrfreg lbh.", "znxr lbh pel", "fnl tbbqolr,", "gryy n yvr naq uheg lbh."]).encode("rot_13")
+
+
